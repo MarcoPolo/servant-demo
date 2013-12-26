3747df4ab713ba6c293c9a430a5f04d0becfa5d9
Marco Munizaga <marco.munizaga@ufl.edu>
2013-12-26 13:32:39 -0500

    updated the core async dependency

--- project.clj
+++ project.clj
@@ -1,17 +1,19 @@
-(defproject servant-demo "0.1.2"
+(defproject servant-demo "0.1.3"
   :description "Demo for servant library"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-cljsbuild "0.3.3"]]
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1859"] 
-                 [org.clojure/core.async "0.1.222.0-83d0c2-alpha"]
-                 [servant "0.1.3"] ]
+                 [org.clojure/clojurescript "0.0-2127"] 
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+                 [servant "0.1.4"]
+                 ]
   :cljsbuild
               {:builds
                [{:id "servant-demo"
                  :source-paths ["src/cljs/servant-demo"]
                  :compiler {:optimizations :simple
                             :pretty-print false
-                            :output-to "main.js" }}]})
+                            :output-to "main.js"
+                            :source-map "main.js.map"}}]})
